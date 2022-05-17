6eff1dde54dc004a110c7d008d909a460e811055
Hemanthsivaram <97418019+Hemanthsivaram@users.noreply.github.com>
2022-05-17 18:37:05 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://15.207.18.248:9000/',
+  serverUrl: 'http://13.232.74.154:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
